334c55cc6df2237fc68b6c361690b5cca262b247
justhineTumbaco <justhinej@hotmail.com>
2016-08-11 03:34:23 -0500

    Implement Camara

---
+++ app/src/main/java/com/injob/injob/injobapp/EditEmp.java
@@ -0,0 +1,13 @@
+package com.injob.injob.injobapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditEmp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_emp);
+    }
+}
--- app/src/main/java/com/injob/injob/injobapp/HomeEmpleados.java
+++ app/src/main/java/com/injob/injob/injobapp/HomeEmpleados.java
@@ -1,18 +1,28 @@
 package com.injob.injob.injobapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
 
 public class HomeEmpleados extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, ZXingScannerView.ResultHandler {
 
+    private ZXingScannerView mScannerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +35,25 @@ public class HomeEmpleados extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+
+    public void QrScanner(View view) {
+        mScannerView=new ZXingScannerView(this);
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -71,6 +94,9 @@ public class HomeEmpleados extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
+            
+
+
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
@@ -87,5 +113,25 @@ public class HomeEmpleados extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        AlertDialog.Builder builder= new  AlertDialog.Builder(this);
+        builder.setTitle("scan Result");
+        builder.setMessage(result.getText());
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                aceptar();
+            }
+        });
+        AlertDialog alert1= builder.create();
+        alert1.show();
+
+    }
+    public void aceptar(){
+        Intent i = new Intent(this, HomeEmpleados.class);
+        startActivity(i);
     }
 }
