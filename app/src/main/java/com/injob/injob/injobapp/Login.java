dec9a3fea61abd7abc5b5c9071f0ed8885963524
justhineTumbaco <justhinej@hotmail.com>
2016-08-19 21:07:28 -0500

    Web service and Login

--- app/src/main/java/com/injob/injob/injobapp/Login.java
+++ app/src/main/java/com/injob/injob/injobapp/Login.java
@@ -1,19 +1,39 @@
 package com.injob.injob.injobapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TabHost;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Login extends AppCompatActivity {
     //TabHost tabHost;
+    Button btnIngresar;
+    EditText txtusu,txtpas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtusu=(EditText)findViewById(R.id.txt_Email);
+        txtpas=(EditText)findViewById(R.id.txt_Pass);
+        btnIngresar=(Button)findViewById(R.id.btn_submit);
+
 
         TabHost host = (TabHost)findViewById(R.id.tabHost);
         host.setup();
@@ -32,10 +52,71 @@ public class Login extends AppCompatActivity {
 
 
     }
+    public String enviarDatosGet(String usu, String pas){
+
+        URL url =null;
+        String line="";
+        int respuesta=0;
+        StringBuilder resul = null;
+
+        try {
+            url= new URL("http://169.254.217.96/injob/valida.php?usu="+usu+"&pas="+pas);
+
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            respuesta= connection.getResponseCode();
+            resul= new StringBuilder();
+
+            if(respuesta== HttpURLConnection.HTTP_OK){
+                InputStream in = new BufferedInputStream(connection.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                while ((line= reader.readLine())!=null){
+                    resul.append(line);
+                }
+
+            }
+        }catch (Exception e){
+
+        }
+        return resul.toString();
+
+    }
+    public int obtDatosJson(String response){
+        int res=0;
+        try {
+            JSONArray json= new JSONArray(response);
+            if(json.length()>0){
+                res=1;
+            }
+        }catch (Exception e){
+        }
+        return res;
+    }
+
     public void login(View view){
-        Intent i = new Intent(this, HomeEmpleados.class);
-        startActivity(i);
-    }public void loginAdmin(View view){
+        Thread tr= new Thread() {
+            @Override
+            public void run() {
+                final String resultado = enviarDatosGet(txtusu.getText().toString(), txtpas.getText().toString());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int r = obtDatosJson(resultado);
+                        if (r > 0) {
+                            Intent i = new Intent(getApplicationContext(), HomeEmpleados.class);
+                            i.putExtra("cod", txtusu.getText().toString());
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "usuario o pass mal", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        };
+            tr.start();
+        }
+
+    public void loginAdmin(View view){
         Intent i = new Intent(this, Admin.class);
         startActivity(i);
     }
